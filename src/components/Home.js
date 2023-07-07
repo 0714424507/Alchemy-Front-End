5b948ea2d5e4232a424552838e747bd29de8e0d1
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-07-07 07:36:54 +0300

    Add Carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import HomeCarousel from './Carousel';
+import '../App.css';
 
 function Home() {
   return (
     <div>
-      <h2 className='top'>A L C H E M Y </h2>
-      <h2 className='bottom'>A L C H E M Y</h2>
+      <HomeCarousel />
       <h1 className='title-top'>A L C H E M Y</h1>
       <h1 className='title-bottom'>A L C H E M Y</h1>
     </div>
   );
 }
+
 export default Home;
