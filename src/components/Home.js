09c7af38fef6cc0ed97d33b67f6cb40b08a770c1
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-07-07 07:56:16 +0300

    Add Carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ function Home() {
   return (
     <div>
       <HomeCarousel />
+      <h3 className='slogan'>The Best Version of You</h3>
       <h1 className='title-top'>A L C H E M Y</h1>
       <h1 className='title-bottom'>A L C H E M Y</h1>
     </div>
